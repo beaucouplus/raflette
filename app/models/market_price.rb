38d90f309f33893c64a8d44a8c109bc27ebc9af6
Maxime Souillat <maxime@beaucouplus.com>
2024-11-19 16:19:04 +0100

    market prices > create by_date scope and validations

--- app/models/market_price.rb
+++ app/models/market_price.rb
@@ -1,2 +1,8 @@
 class MarketPrice < ApplicationRecord
+
+  scope :by_date, -> (date) { where(time: date.at_beginning_of_day..date.at_end_of_day) }
+
+  validates :time, presence: true, uniqueness: true
+  validates :value, presence: true
+
 end
--- test/models/market_price_test.rb
+++ test/models/market_price_test.rb
@@ -1,7 +1,41 @@
 require "test_helper"
 
 class MarketPriceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @date = Date.current
+    @default_value = 100.10
+  end
+
+  test "by_date scope returns prices within the same day, even at the boundaries" do
+    market_price_lower_boundary = MarketPrice.create(time: @date.beginning_of_day, value: @default_value)
+    market_price_inside_boundary = MarketPrice.create(time: @date.beginning_of_day + 3.hours, value: @default_value)
+    market_price_upper_boundary = MarketPrice.create(time: @date.end_of_day, value: @default_value)
+
+    market_price_outside_boundary = MarketPrice.create(time: @date.yesterday, value: @default_value)
+
+    results = MarketPrice.by_date(@date)
+
+    assert_includes results, market_price_lower_boundary
+    assert_includes results, market_price_inside_boundary
+    assert_includes results, market_price_upper_boundary
+    refute_includes results, market_price_outside_boundary
+  end
+
+  test "by_date scope returns empty collection for date with no prices" do
+    assert_equal [], MarketPrice.by_date(Date.current + 1.year)
+  end
+
+  test "2 prices cannot have the same time" do
+    time = Time.current
+    first_price = MarketPrice.create(time: time, value: @default_value)
+    assert first_price.persisted?
+
+    duplicate_price = MarketPrice.new(time: time, value: @default_value)
+    refute duplicate_price.valid?
+  end
+
+  test "value must be present" do
+    market_price = MarketPrice.new(value: nil)
+    refute market_price.valid?
+  end
 end
