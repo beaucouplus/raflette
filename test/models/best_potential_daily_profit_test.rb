1b64e7d4954a074b793d9071638eaf8d158a7c19
Maxime Souillat <maxime@beaucouplus.com>
2024-11-21 14:30:46 +0100

    best potential daily profits > add test and simplify

--- test/models/best_potential_daily_profit_test.rb
+++ test/models/best_potential_daily_profit_test.rb
@@ -1,26 +1,36 @@
 require "test_helper"
 
 class BestPotentialDailyProfitTest < ActiveSupport::TestCase
-  setup do
-    @two_days_ago = 2.days.ago.at_beginning_of_day
-  end
-
   test "it returns the maximum potential profit" do
     prices = [
-      MarketPrice.new(time: @two_days_ago, value: 100.25 ),
-      MarketPrice.new(time: @two_days_ago.change(hours: 2, minutes: 25), value: 100.29 ),
-      MarketPrice.new(time: @two_days_ago.change(hours: 2, minutes: 28), value: 100.29 ),
-      MarketPrice.new(time: @two_days_ago.change(hours: 2, minutes: 29), value: 100.29 ),
+      MarketPrice.new(value: 100.25),
+      MarketPrice.new(value: 100.29),
+      MarketPrice.new(value: 100.29),
+      MarketPrice.new(value: 100.29)
     ]
 
     potential_profit = BestPotentialDailyProfit.new(prices).find
     assert_equal 4, potential_profit
   end
 
+  test "it returns the maximum potential profit even in a complex setup" do
+    prices = [
+      MarketPrice.new(value: 100.29),
+      MarketPrice.new(value: 100.28),
+      MarketPrice.new(value: 100.25),
+      MarketPrice.new(value: 100.27),
+      MarketPrice.new(value: 100.35),
+      MarketPrice.new(value: 100.24)
+    ]
+
+    potential_profit = BestPotentialDailyProfit.new(prices).find
+    assert_equal 10, potential_profit
+  end
+
   test "it returns 0 if there is no profit possible" do
     prices = [
-      MarketPrice.new(time: @two_days_ago, value: 100.29 ),
-      MarketPrice.new(time: @two_days_ago.change(hours: 2, minutes: 25), value: 100.25 )
+      MarketPrice.new(value: 100.29),
+      MarketPrice.new(value: 100.25)
     ]
 
     potential_profit = BestPotentialDailyProfit.new(prices).find
